f4b60da99a51f02fc01ce816cdfba761a7ce8a4e
Omith Dewsara <OmithDewsara123@gmail.com>
2021-09-05 08:00:50 -0400

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const cors = require("cors");
 const stripe = require("stripe")(
   "sk_test_51JRvZgBB7TaVdvcQ2YIPUfLpEm43hOr5XqLTGuiEEcIyNDQY1Bw1OIYZE7s9klC3QmpvkEmITUamZBrBSgTM8g9A00IUZXZ07O");
 
@@ -7,7 +6,6 @@ const app = express();
 
 //middlewares
 app.use(express.json());
-app.use(cors({ origin: true }))
 const port = process.env.PORT || 5000;
 
 //api routes
